82779ee6f304ca071a5d6382298433a23a9fe560
Lovro Habjan <lh5107@student.uni-lj.si>
2021-03-30 22:25:12 +0200

    Fix mistake with line condition, formatting
    The end and over condition were wrong for line clauses.
    
    Formatted the code a bit, especially createOneEntry.

--- kraljice.hs
+++ kraljice.hs
@@ -2,48 +2,56 @@ import System.Environment
 import Debug.Trace
 
 
-
-createOneEntry isOver isEnd increment root i acc =
-  let item = [-root, -i]
+createOneEntry isOver isEnd increment root =
+  let call i acc =
+        let item = [-root, -i]
+        in
+          if isOver i then
+            reverse acc
+          else if isEnd i then
+            reverse (item : acc)
+          else
+            call (increment i) (item : acc)
   in
-    if isOver i then
-      reverse acc
-    else if isEnd i then
-      reverse (item : acc)
-    else
-      createOneEntry isOver isEnd increment root (increment i) (item : acc)
+    call (increment root) []
+
 
 createLines n startLine  =
-  let endLine = startLine + n - 2
-      indexes = [startLine .. endLine]
-      lines = map (\i -> createOneEntry (\x -> n < x) (\x -> n == x) (\i -> i + 1) i (i + 1) []) indexes
+  let endLine = startLine + n - 1
+      indexes = [startLine .. endLine - 1]
+      increment x = x + 1
+      lines = [createOneEntry (\x -> endLine < x) (\x -> endLine == x) increment i | i <- indexes]
   in
     concat lines
 
+
 createColumns n startColumn =
-  let indexes = map (\i -> n * (i-1) + startColumn) [1 .. n - 1]
+  let indexes = [n * (i - 1) + startColumn | i <- [1 .. n - 1]]
       endColumn = (n-1) * n + startColumn
-      columns = map (\i -> createOneEntry (\x -> endColumn < x) (\x -> endColumn == x) (\x -> x + n) i (i + n) []) indexes
+      increment x = x + n
+      columns = [createOneEntry (\x -> endColumn < x) (\x -> endColumn == x) increment i | i <- indexes]
   in
     concat columns
 
 
 createDiagonalOne n startIndex =
-  let indexes = map (\i -> startIndex + (i - 1) * (n + 1)) [1 .. (n - (startIndex `mod` n))]
-      diagonals = map (\i -> createOneEntry (\x -> x > n * n) (\x -> x `mod` n == 0) (\x -> x + n + 1) i (i + n + 1) []) indexes
+  let indexes = [startIndex + (i - 1) * (n + 1) | i <- [1 .. (n - (startIndex `mod` n))]]
+      increment x = x + n + 1
+      diagonals = [createOneEntry (\x -> x > n * n) (\x -> x `mod` n == 0) increment i | i <- indexes]
   in
     concat diagonals
 
 createDiagonalTwo n startIndex =
   let indexes = map (\i -> startIndex + (i - 1) * (n - 1)) [1 .. (startIndex - 1) `mod` n]
-      diagonals = map (\i -> createOneEntry (\x -> x > n * n) (\x -> x `mod` n == 1) (\x -> x + n - 1) i (i + n - 1) []) indexes
+      increment x = x + n - 1
+      diagonals = [createOneEntry (\x -> x > n * n) (\x -> x `mod` n == 1)  increment i | i <- indexes]
   in
     concat diagonals
 
 atLeastOneClause :: Int -> [[Int]]
 atLeastOneClause n =
-  let lines = map (\x -> [(x-1)*n+1 .. x*n]) [1..n]
-      columns = map (\x -> map (\i -> n * (i-1) + x) [1 .. n]) [1 .. n]
+  let lines = [[(x - 1) * n + 1 .. x * n] | x <- [1 .. n]]
+      columns = [map (\i -> n * (i - 1) + x) [1 .. n] | x <- [1 .. n]]
   in
     lines ++ columns
 
